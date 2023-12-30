4a584ac3d5533d241baa8dfb7d647d637d5bf8ce
Rodolfo Suaki <rmsuaki@hotmail.com>
2023-12-29 21:28:02 -0300

    Arrumando o código

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+        static float num1;
+        static float num2;
+        static float num3;
+        static float num4;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner entrada1 = new Scanner(System.in);
+        System.out.println("Digite o número 1: ");
+        num1 = entrada1.nextInt();
+
+        Scanner entrada2 = new Scanner(System.in);
+        System.out.println("Digite o número 1: ");
+        num2 = entrada2.nextInt();
+
+        Scanner entrada3 = new Scanner(System.in);
+        System.out.println("Digite o número 1: ");
+        num3 = entrada3.nextInt();
+
+        Scanner entrada4 = new Scanner(System.in);
+        System.out.println("Digite o número 1: ");
+        num4 = entrada4.nextInt();
+
+        System.out.println("\n A média dos 4 números é: " + calculoMedia());
+
+}
+
+    public static float calculoMedia() {
+
+        return (num1+num2+num3+num4)/4;
     }
 }
+
